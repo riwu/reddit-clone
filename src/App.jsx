969ab068119b45f2c5fb8a08a6411368c447a027
riwu <riwu0730@gmail.com>
2017-10-12 13:13:13 +0800

    Eject CRA and add css modules, hot reload and other packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div styleName="App">
+    <header styleName="App-header">
+      <img src={logo} styleName="App-logo" alt="logo" />
+      <h1 styleName="App-title">Welcome to React</h1>
+    </header>
+    <p styleName="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const render = (Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'));
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
+
+registerServiceWorker();
